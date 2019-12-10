2a81f80319e1fcd7c4a0d1a0245c5538c7b6aced
Abenayan <abenayak@uio.no>
2019-12-10 02:47:42 -0800

    Initial Commit

---
+++ ProjectHackLifeFinal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectHackLifeFinal
+//
+//  Created by Abenayan Ketheeswaran on 12/10/19.
+//  Copyright © 2019 THE WINDOW SPECIALISTS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
